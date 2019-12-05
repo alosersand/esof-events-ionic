47dbbd37674b15045a665e15e30eb8a668070106
lazyBit99 <alex.marchi99@gmail.com>
2019-12-05 19:55:11 +0100

    add custom marker and popup

--- src/map.js
+++ src/map.js
@@ -1,5 +1,6 @@
 const DATA = require('./data.js')
-const eventsPOI = DATA.map.geojson
+const geoJSON = DATA.map.geoJSON
+
 module.exports = {
 	name: 'Map',
 	version: '0.1',
@@ -9,14 +10,42 @@ module.exports = {
 		// MAP INIT
 		// crea mappa webgis
 		const myMap = L.map('mapView').setView([45.6522854, 13.7661518], 14)
+
 		// aggiungo la basemap
-		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
 			attribution:
 				'Power by: ITS - Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
 			maxZoom: 18,
 		}).addTo(myMap)
+
+
+
 		// aggiungo i POI
-		L.geoJSON(eventsPOI).addTo(myMap)
+		L.geoJSON(geoJSON, {
+			pointToLayer: (feature, latlng) => {
+				return L.marker(latlng, {
+					icon: L.icon({
+						iconUrl: feature.properties.icon,
+						iconAnchor: L.point(15, 35)
+					}),
+				})
+			},
+			onEachFeature: (feature, layer) => {
+
+				let popupText = feature.properties.name;
+	
+				layer.bindPopup(popupText, {
+					offset: L.point(0, -30),
+					className: 'popup'
+				});
+				layer.on('click', function() {
+					layer.openPopup();
+				});
+			},
+		}).addTo(myMap)
+
+
+
 		// ascolto il cambio di tabs
 		const tabs = document.querySelector('#tabs')
 		tabs.addEventListener('ionTabsDidChange', event => {
