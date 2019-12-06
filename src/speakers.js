f9e7c6371d8c411f7c44e91cd6c6bb8d00fdf3b3
lazyBit99 <alex.marchi99@gmail.com>
2019-12-06 20:06:48 +0100

    batter speakers section

--- src/speakers.js
+++ src/speakers.js
@@ -3,35 +3,55 @@ const speakerData = DATA.speakers
 const speakerList = document.querySelector('#speaker-list')
 
 module.exports = {
-  name: 'Speaker',
-  version: "0.1",
-  render: () => {
-    console.log('*** Render Speaker ***')
-    speakerList.innerHTML = `
-    ${speakerData.map(item => `
+	name: 'Speaker',
+	version: '0.1',
+	render: () => {
+		console.log('*** Render Speaker ***')
+		speakerList.innerHTML = `
+    ${speakerData
+			.map(
+				speaker => `
       <ion-col size="12" size-md="6">
         <ion-card class="speaker-card">
-          <ion-card-header>
+
+          <ion-card-header class="card-header">
             <ion-item detail="false" lines="none">
+
               <ion-avatar slot="start">
-                  <img src="${item.img}">
+                  <img src="${speaker.img}">
               </ion-avatar>
-              ${item.nome}
+
+              <ion-list>
+                <ion-label class="title">${speaker.nome}</ion-label>
+                <ion-label>${speaker.desc}</ion-label>
+              </ion-list>
+              
             </ion-item>
           </ion-card-header>
-          <ion-card-content>
-            <ion-list>
-              <ion-item>
-                <h3>${item.intervento}: ${item.inizio} &mdash; ${item.fine}</h3>
-              </ion-item>
-              <ion-item>
-                  <p>${item.desc}</p>
-              </ion-item>
+
+          <ion-card-content class="card-content">
+          <ion-item class="title">Interventi</ion-item>
+
+          <ion-list lines="none">
+          ${speaker.interventi
+						.map(
+							intervento => `
+          
+          <ion-item>
+            <span class="bull">&bull;&nbsp;</span>${intervento}
+          </ion-item>
+          `
+						)
+						.join('\n')}
+
+              
             </ion-list>
           </ion-card-content>
         </ion-card>
       </ion-col>
-          `).join('\n')}
+          `
+			)
+			.join('\n')}
       `
-  }
+	},
 }
